4bb205dc57938cdfc408a91f5c0662377c2d6186
ahmadkhwaja58 <ahmadkhwaja58@gmail.com>
2026-02-17 12:30:48 +0500

    Create config.py for configuration management

---
+++ config.py
@@ -0,0 +1,22 @@
+# Configuration Management
+
+# API Keys
+
+# Amazon Associates
+AMAZON_ASSOCIATES_API_KEY = 'your_amazon_api_key_here'
+
+# ShareASale
+SHAREASALE_API_KEY = 'your_shareasale_api_key_here'
+
+# Social Media Platforms
+FACEBOOK_API_KEY = 'your_facebook_api_key_here'
+TWITTER_API_KEY = 'your_twitter_api_key_here'
+INSTAGRAM_API_KEY = 'your_instagram_api_key_here'
+
+# Email Settings
+EMAIL_HOST = 'your_email_host_here'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'your_email_host_user_here'
+EMAIL_HOST_PASSWORD = 'your_email_host_password_here'
+
+# Additional settings can be added as needed
